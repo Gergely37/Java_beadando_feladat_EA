ab6fa4f298933e8428fd514569e02737677bc140
Gergely37 <87493677+Gergely37@users.noreply.github.com>
2022-12-04 18:25:07 +0100

    Generátorok hozzáfüzése

--- beadando_EA/src/main/java/com/example/beadando_ea/MainProg.java
+++
@@ -1,7 +0,0 @@
-package com.example.beadando_ea;
-
-public class MainProg {
-    public static void main(String[] args){
-        HelloApplication.main(args);
-    }
-}
--- beadando_EA/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.beadando_ea {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.beadando_ea to javafx.fxml;
-    exports com.example.beadando_ea;
-}
